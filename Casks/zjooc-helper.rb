a6aacd441c1b01456f39f42b11a2e29ef1784c4a
ZacharyWin <72899968+ZacharyWin@users.noreply.github.com>
2022-04-24 21:17:36 +0800

    Create zjooc-helper.rb

---
+++ Casks/zjooc-helper.rb
@@ -0,0 +1,12 @@
+cask 'zjooc-helper' do
+  version 'x.x.x'
+  sha256 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+
+  url "https://github.com/ZacharyWin/swift-ZjoocHelper/releases/download/v#{version}/ZjoocHelper.dmg"
+  appcast 'https://github.com/ZacharyWin/swift-ZjoocHelper/releases.atom'
+  name 'ZjoocHelper'
+  homepage 'https://github.com/ZacharyWin/swift-ZjoocHelper'
+
+  app 'Chuck ZjoocHelper.app'
+
+end
