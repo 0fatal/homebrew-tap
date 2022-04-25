18bdf9f052006cf8f90a55007605a5f5ced20498
ZacharyWin <ZacharyWin@users.noreply.github.com>
2022-04-25 14:17:25 +0000

    update ZjoocHelper to v0.0.1

--- Casks/ZjoocHelper.rb
+++ Casks/ZjoocHelper.rb
@@ -1,6 +1,6 @@
 cask 'ZjoocHelper' do
-  version 'x.x.x'
-  sha256 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+  version '0.0.1'
+  sha256 '9edb3cb59e276ec80eba49084e0c08f2f708a5a3c0600aee65e35e0225971491'
 
   url "https://github.com/ZacharyWin/swift-ZjoocHelper/releases/download/v#{version}/ZjoocHelper.dmg"
   appcast 'https://github.com/ZacharyWin/swift-ZjoocHelper/releases.atom'
