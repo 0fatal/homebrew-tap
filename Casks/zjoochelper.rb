be6090afd311c6b45acd92f08161e061487808f0
0xfatal <72899968+0xfatal@users.noreply.github.com>
2022-07-20 19:56:00 +0800

    Update zjoochelper.rb

--- Casks/zjoochelper.rb
+++ Casks/zjoochelper.rb
@@ -2,10 +2,10 @@ cask 'zjoochelper' do
   version '0.0.1'
   sha256 '9edb3cb59e276ec80eba49084e0c08f2f708a5a3c0600aee65e35e0225971491'
 
-  url "https://github.com/ZacharyWin/swift-ZjoocHelper/releases/download/v#{version}/ZjoocHelper.dmg"
-  appcast 'https://github.com/ZacharyWin/swift-ZjoocHelper/releases.atom'
+  url "https://github.com/0xfatal/swift-ZjoocHelper/releases/download/v#{version}/ZjoocHelper.dmg"
+  appcast 'https://github.com/0xfatal/swift-ZjoocHelper/releases.atom'
   name 'ZjoocHelper'
-  homepage 'https://github.com/ZacharyWin/swift-ZjoocHelper'
+  homepage 'https://github.com/0xfatal/swift-ZjoocHelper'
 
   app 'ZjoocHelper.app'
 
